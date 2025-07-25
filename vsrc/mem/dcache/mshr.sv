fb4ade8a39aeae680f8c9d40bdb0d2b1cfeceb88
Jpz-sdju <rengeyouzh1@163.com>
2025-07-25 19:22:56 +0800

    feat[MSHR]:add mshr entry

--- vsrc/mem/dcache/mshr.sv
+++ vsrc/mem/dcache/mshr.sv
@@ -10,6 +10,39 @@ module mshr (
     input wire [`PADDR_RANGE] allocate2_paddr,
 
 
+    output wire dmshr2arb_valid,
+    input wire dmshr2arb_ready,
+    output wire [`PADDR_RANGE] dmshr2arb_paddr,
+
 );
 
+
+
+
+
+
+    genvar i;
+    generate
+        for(i = 0; i < `MSHR_NUM; i = i + 1) begin : 
+            mshr_entry #(
+                .MSHR_ID(i)
+            )mshr_entry_inst (
+                .clock(clock),
+                .reset_n(reset_n),
+                .allocate0_valid(allocate0_valid && (i == 0)),
+                .allocate0_paddr(allocate0_paddr),
+                .allocate1_valid(allocate1_valid && (i == 1)),
+                .allocate1_paddr(allocate1_paddr),
+                .allocate2_valid(allocate2_valid && (i == 2)),
+                .allocate2_paddr(allocate2_paddr),
+                .dmshr2arb_valid(dmshr2arb_valid && (i == 0)),
+                .dmshr2arb_ready(dmshr2arb_ready),
+                .dmshr2arb_paddr(dmshr2arb_paddr)
+            );
+
+
+
+            
+        end
+    endgenerate
 endmodule
--- vsrc/mem/dcache/mshr_entry.sv
+++ vsrc/mem/dcache/mshr_entry.sv
@@ -1,5 +1,7 @@
 `include "defines.sv"
-module mshr_entry (
+module mshr_entry#(
+    parameter MSHR_ID = 0
+) (
     input wire                clock,          // Clock signal
     input wire                reset_n,        // Active low reset
     input wire                install_valid,
@@ -8,4 +10,16 @@ module mshr_entry (
 
 );
 
+
+    // Internal signals for MSHR entry
+    wire [`PADDR_RANGE] mshr_paddr;
+    wire mshr_valid;
+
+    // Logic to handle MSHR entry operations
+    assign mshr_paddr = install_paddr;
+    assign mshr_valid = install_valid;
+
+    // Output signals for the MSHR entry
+    assign dmshr2arb_valid = mshr_valid;
+    assign dmshr2arb_paddr = mshr_paddr;
 endmodule

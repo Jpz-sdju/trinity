513f5cb56376eb65a272c212a07854d2076ce73c
jpz <rengeyouzh1@163.com>
2025-07-26 20:43:17 +0800

    feat[lpipe2]:add some logic

--- vsrc/mem/dcache/dcache_loadpipe_l2.sv
+++ vsrc/mem/dcache/dcache_loadpipe_l2.sv
@@ -9,24 +9,33 @@ module dcache_loadpipe_l2 #(
     input wire reset_n,  // Active low reset
     input wire flush,
 
-    input  wire                froml1_req_valid,
-    output wire                froml1_req_ready,
-    input  wire [`VADDR_RANGE] froml1_req_vaddr,
+    input  wire                   froml1_req_valid,
+    output wire                   froml1_req_ready,
+    input  wire [   `VADDR_RANGE] froml1_req_vaddr,
+    input  wire [`ROB_SIZE_LOG:0] froml1_req_robid,
+    // input  wire [`SQ_SIZE_LOG:0] froml1_req_sqid,
+
 
     input wire                fromtlb_valid,
+    input wire                fromtlb_hit,
     input wire [`PADDR_RANGE] fromtlb_paddr,
 
     input wire [TAGARRAY_DATA_WIDTH-1:0] tagarray_rd_data[0:`DCACHE_WAY_NUM-1],
 
 
 
-    output wire                           tagarray_rd_en,
-    output wire [TAGARRAY_ADDR_WIDTH-1:0] tagarray_rd_idx
+    output wire                   mshr_allocate_valid,
+    input  wire                   mshr_allocate_ready,
+    output wire [   `PADDR_RANGE] mshr_allocate_paddr,
+    output wire [`ROB_SIZE_LOG:0] mshr_allocate_robid
 
 );
 
     /* ------------------------- lookup logic (s1)------------------------- */
     reg [`DCACHE_WAY_NUM-1:0] hitway;
+    //means we lookup hit or miss is valid.
+    wire froml1_is_valid;
+    assign froml1_is_valid = froml1_req_valid & fromtlb_valid & fromtlb_hit;
 
     always @(*) begin
         integer i;
@@ -41,10 +50,12 @@ module dcache_loadpipe_l2 #(
         end
     end
 
-    wire lookup_hit = |hitway;
-    wire lookup_miss = !lookup_hit;
+    wire lookup_hit = |hitway & froml1_is_valid;
+    wire lookup_miss = !lookup_hit & froml1_is_valid;
 
-    
+
+    //Only when we have paddr,we should allocate MSHR.
+    assign mshr_allocate_valid = lookup_miss ;
 
 
 endmodule
